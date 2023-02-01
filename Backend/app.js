7d6dca2c69d1eceb3ec333a8ebd155d3edccf018
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-02-01 18:58:17 +0530

    i am adding backebd

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,23 @@ app.use("/api/v1",product);
 app.use("/api/v1",user)
 app.use(express.json())
 app.use(cookieParser())
+app.get("/set-cookie", (req, res) => {
+    res.cookie("jwt", "your-token-value");
+    res.send("Cookie set");
+  });
+  
+app.get("/get-cookie", (req, res) => {
+ const token = req.cookies.jwt;
+    if (!token) {
+      return res.send("Cookie not found");
+    }
+  
+    res.send(`Cookie value: ${token}`);
+  });
+  
+app.listen(3000, () => {
+    console.log("Server started on http://localhost:3000");
+  });
 //middle ware
 app.use(errorMiddleware)
 module.exports=app
--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -4,8 +4,9 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  const token  = req.cookies;
-
+  req.cookies = req.cookies || {};
+  const token  = req.cookies.jwt;
+  console.log(token)
   if (!token) {
     return next(new ErrorHander("Please Login to access this resource", 401));
   }
@@ -16,18 +17,3 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 
   next();
 });
-
-exports.authorizeRoles = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ErrorHander(
-          `Role: ${req.user.role} is not allowed to access this resouce `,
-          403
-        )
-      );
-    }
-
-    next();
-  };
-};
